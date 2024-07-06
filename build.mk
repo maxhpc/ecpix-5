8de4bc8b719ab975f2d27e9b6a295eae2d3e9fed
Maxim Vorontsov <max@maxhpc.ai>
2024-07-06 21:08:57 +0200

    ddrW and ddrR uart functionality finished and tested...

--- build.mk
+++ build.mk
@@ -6,8 +6,11 @@ ${PROJECT}.bit: ${PROJECT}.config
 ${PROJECT}.config: ${PROJECT}.json
 	nextpnr-ecp5 --json $< --textcfg $@ --um5g-85k --lpf ../ecpix5.lpf --package CABGA554 --freq 100 --lpf-allow-unconstrained
 
-${PROJECT}.json: $(SRC)
+${PROJECT}.json: project.v
 	yosys -p "synth_ecp5 -json $@" $<
 
+project.v: $(SRC)
+	sv2v $< > $@
+
 clean:
-	rm ${PROJECT}.bit ${PROJECT}.config ${PROJECT}.json
+	rm project.v ${PROJECT}.bit ${PROJECT}.config ${PROJECT}.json
--- vx/vx.sv
+++ vx/vx.sv
@@ -14,7 +14,7 @@
 
 `define LENGTH(ADR,SIZ,OFS) (((ADR+SIZ-1)>>(OFS))-((ADR)>>(OFS)))
 
-module vortex(
+module vx(
  `include "../ecpix5_ports.hv"
 );
 /* clocks */
@@ -84,7 +84,7 @@ module vortex(
  //
  reg[31:0] axi4_araddr;
  reg [7:0] axi4_arlen;
- reg       axi4_arvalid;
+ logic     axi4_arvalid;
  wire      axi4_arready;
  //
  wire[31:0] axi4_rdata;
@@ -179,14 +179,14 @@ wire       dfi_rddata_valid_w;
        ,RCV_CMD_STB
        ,RCV_WRWORD
        ,RCV_DDRWR
-       ,RCV_RDWORD
        ,RCVST_SIZE} RCVST_ENUM;
   reg[RCVST_SIZE-1:0] rcv_state;
-  reg           [6:0] rcv_byteCnt;
-  reg       [9*8-1:0] rcv_cmd;
-   wire [7:0] rcv_opcode = rcv_cmd[0+:8];
-   wire[31:0] rcv_addr   = rcv_cmd[8+:32];
-   wire[31:0] rcv_size   = rcv_cmd[(8+32)+:32];
+  struct packed{
+   reg   [31:0] size;
+   reg   [31:0] addr;
+   reg[4*8-1:0] opcode;
+  } rcv_cmd;
+  reg[6:0] rcv_cmdCnt;
   //
   reg[32:0] rcv_wrCnt;
   reg [1:0] rcv_wrPtr;
@@ -195,57 +195,56 @@ wire       dfi_rddata_valid_w;
    if (!rst_) begin
     axi4_awvalid <= 1'b0;
     axi4_wvalid  <= 1'b0;
-    axi4_arvalid <= 1'b0;
     //
-    rcv_byteCnt <= 9 -2;
+    rcv_cmdCnt <= ($bits(rcv_cmd)+7)/8 -2;
     rcv_state <= (1<<RCV_CMD);
    end
     else begin
      if (axi4_awready) begin
       axi4_awvalid <= 1'b0;
-      axi4_awaddr <= axi4_awaddr+1;
+      if (axi4_awvalid) begin
+       axi4_awaddr <= axi4_awaddr+4;
+      end
      end
      if (axi4_wready) begin
       axi4_wvalid <= 1'b0;
-     end
-     if (axi4_arready) begin
-      axi4_arvalid <= 1'b0;
-      axi4_araddr <= axi4_araddr+1;
+      axi4_wstrb <= 4'b1111;
      end
      unique case (rcv_state)
       default: begin // (1<<RCV_CMD)
        if (rx_stb) begin
-        rcv_cmd[9*8-1-:8] <= rx_byte;
-        rcv_cmd[0+:8*8] <= rcv_cmd[9*8-1-:8*8];
+        rcv_cmd[$bits(rcv_cmd)-1-:8] <= rx_byte;
+        rcv_cmd[0+:$bits(rcv_cmd)-8] <= rcv_cmd[$bits(rcv_cmd)-1-:$bits(rcv_cmd)-8];
         //
-        if (rcv_byteCnt[6]) begin
+        if (rcv_cmdCnt[6]) begin
          rcv_state <= (1<<RCV_CMD_STB);
         end
-        rcv_byteCnt <= rcv_byteCnt-1;
+        rcv_cmdCnt <= rcv_cmdCnt-1;
        end
       end
       (1<<RCV_CMD_STB): begin
-       rcv_wrCnt <= rcv_size -2;
-       rcv_wrPtr <= rcv_addr[1:0];
+       rcv_wrCnt <= rcv_cmd.size -2;
+       rcv_wrPtr <= rcv_cmd.addr[1:0];
        //
-       axi4_wstrb  <= 4'b1111<<rcv_addr[1:0];
+       axi4_awaddr  <= rcv_cmd.addr[31:2]<<2;
+       axi4_wstrb  <= 4'b1111<<rcv_cmd.addr[1:0];
        //
-       case (rcv_opcode)
-        default: begin
-         rcv_byteCnt <= 9 -2;
-         rcv_state <= (1<<RCV_CMD);
-        end
-        "w": begin
+       case (rcv_cmd.opcode)
+        "ddrW": begin
          rcv_state <= (1<<RCV_WRWORD);
         end
-        "r": begin
-         rcv_state <= (1<<RCV_RDWORD);
+        //
+        default: begin
+         rcv_cmdCnt <= ($bits(rcv_cmd)+7)/8 -2;
+         rcv_state <= (1<<RCV_CMD);
         end
        endcase
       end
       //
       (1<<RCV_WRWORD): begin
        if (rx_stb) begin
+        rcv_wrCnt <= rcv_wrCnt-1;
+        rcv_wrPtr <= rcv_wrPtr+1;
         case (rcv_wrPtr)
          2'h0: begin
           rcv_wrWord[0+:8] <= rx_byte;
@@ -260,65 +259,105 @@ wire       dfi_rddata_valid_w;
           rcv_wrWord[24+:8] <= rx_byte;
          end
         endcase
-        rcv_wrPtr <= rcv_wrPtr+1;
-        //
         if ((rcv_wrPtr==3'h3) || rcv_wrCnt[32]) begin
          rcv_state <= (1<<RCV_DDRWR);
         end
-        rcv_wrCnt <= rcv_wrCnt-1;
        end
       end
       (1<<RCV_DDRWR): begin
-       axi4_awaddr  <= rcv_addr[31:2]<<2;
        axi4_awlen   <= 8'h0;
        axi4_awvalid <= 1'b1;
        //
        axi4_wdata <= rcv_wrWord;
-       rcv_state <= (1<<RCV_WRWORD);
-       if (rcv_wrCnt[32]) begin
-        axi4_wstrb  <= axi4_wstrb & (4'b1111>>(2'h3 & ~(rcv_addr[1:0]+rcv_size[1:0]-1)));
-        //
-        rcv_byteCnt <= 9 -2;
-        rcv_state <= (1<<RCV_CMD);
-       end
        axi4_wlast  <= 1'b1;
        axi4_wvalid <= 1'b1;
-      end
-      //
-      (1<<RCV_RDWORD): begin
-       axi4_araddr  <= rcv_addr[31:2]<<2;
-       axi4_arlen   <= 8'h0;
-       axi4_arvalid <= 1'b1;
        //
-       rcv_byteCnt <= 9 -2;
-       rcv_state <= (1<<RCV_CMD);
+       rcv_state <= (1<<RCV_WRWORD);
+       rcv_cmdCnt <= ($bits(rcv_cmd)+7)/8 -2;
+       if (rcv_wrCnt[32] && !rcv_wrCnt[0]) begin
+        axi4_wstrb  <= axi4_wstrb & (4'b1111>>(2'h3 & ~(rcv_cmd.addr[1:0]+rcv_cmd.size[1:0]-1)));
+        //
+        rcv_state <= (1<<RCV_CMD);
+       end
       end
      endcase
     end
  /**/
  /* send */
-  reg[8*8-1:0] snd_word;
-  reg    [6:0] snd_byteCnt;
+  enum {SND_IDLE
+       ,SND_AXI4_ARVALID
+       ,SND_AXI4_RVALID
+       ,SND_AXI4_RDATA
+       ,SNDST_SIZE} SNDST_ENUM;
+  reg[SNDST_SIZE-1:0] snd_state;
+   assign axi4_arvalid = snd_state[SND_AXI4_ARVALID];
+  reg[31:0] snd_word;
+  reg[32:0] snd_byteCnt;
+  reg [1:0] snd_axi4rdPtr;
   `ALWAYS(posedge sysclk, negedge rst_)
    if (!rst_) begin
     tx_req <= 1'b0;
     //
-    snd_byteCnt <= -1;
+    snd_state <= (1<<SND_IDLE);
    end
     else begin
      if (!tx_idle) begin
       tx_req <= 1'b0;
      end
-     if (axi4_rvalid) begin
-      snd_word <= axi4_rdata;
-      snd_byteCnt <= 4 -1;
-     end
-     if (!snd_byteCnt[6] && tx_idle && !tx_req) begin
-      tx_byte <= snd_word[0+:8];
-      snd_word <= snd_word>>8;
-      tx_req <= 1'b1;
-      snd_byteCnt <= snd_byteCnt-1;
-     end
+     unique case (snd_state)
+      default: begin // (1<<SND_IDLE)
+       if (rcv_state[RCV_CMD_STB]) begin
+        case (rcv_cmd.opcode)
+         "ddrR": begin
+          axi4_araddr <= rcv_cmd.addr[31:2]<<2;
+          axi4_arlen   <= 8'h0;
+          //
+          snd_byteCnt <= rcv_cmd.size -2;
+          snd_axi4rdPtr <= rcv_cmd.addr[1:0];
+          //
+          snd_state <= (1<<SND_AXI4_ARVALID);
+         end
+         //
+         default: begin
+         end
+        endcase
+       end
+      end
+      //
+      (1<<SND_AXI4_ARVALID): begin
+       if (axi4_arready) begin
+        axi4_araddr <= axi4_araddr+4;
+        //
+        snd_state <= (1<<SND_AXI4_RVALID);
+       end
+      end
+      //
+      (1<<SND_AXI4_RVALID): begin
+       if (axi4_rvalid) begin
+        snd_word <= axi4_rdata>>(8*snd_axi4rdPtr);
+        //
+        snd_state <= (1<<SND_AXI4_RDATA);
+       end
+      end
+      //
+      (1<<SND_AXI4_RDATA): begin
+       if (tx_idle && !tx_req) begin
+        snd_byteCnt <= snd_byteCnt-1;
+        snd_axi4rdPtr <= snd_axi4rdPtr+1;
+        //
+        tx_byte <= snd_word[0+:8];
+        snd_word <= snd_word>>8;
+        tx_req <= 1'b1;
+        //
+        if (snd_axi4rdPtr==3'h3) begin
+         snd_state <= (1<<SND_AXI4_ARVALID);
+        end
+        if (snd_byteCnt[32]) begin
+         snd_state <= (1<<SND_IDLE);
+        end
+       end
+      end
+     endcase
     end
  /**/
 /**/
