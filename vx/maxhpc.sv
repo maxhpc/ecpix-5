a9b5db3bc4a71637c42fa6886bf6c14bde2ac5ad
Maxim Vorontsov <max@maxhpc.ai>
2024-07-26 01:02:52 +0200

    ... simple signals tracing ability added (stap)

--- vx/maxhpc.sv
+++ vx/maxhpc.sv
@@ -7,6 +7,8 @@
 `endif
 `include "../libs/ddr3/ddr3_top.sv"
 `include "../libs/uart/uart.sv"
+//
+`include "../libs/maxhpc_dpram.sv"
 
 `define ALWAYS(CLOCK,RESET) \
  `ifndef NOASYNC \
@@ -30,11 +32,10 @@ module maxhpc(
  `ifndef VERILATOR
   wire [3:0] clk_pll;
   wire       pll_locked;
-  ecp5pll
-  #(
+  ecp5pll #(
    .in_hz(100000000)
-  ,.out0_hz(50000000)
-  ,.out1_hz(50000000)
+  ,.out0_hz(25000000)
+  ,.out1_hz(25000000)
   ,.out1_deg(90)
   ) pll(
    .clk_i(fpga_sysclk)
@@ -78,8 +79,69 @@ module maxhpc(
    end
 /**/
 
+/* Vortex instance */
+ reg vx_rst;
+ // Memory request
+ wire                           mem_req_valid;
+ wire                           mem_req_rw;
+ wire[`VX_MEM_BYTEEN_WIDTH-1:0] mem_req_byteen;
+ wire[  `VX_MEM_ADDR_WIDTH-1:0] mem_req_addr;
+ wire[  `VX_MEM_DATA_WIDTH-1:0] mem_req_data;
+ wire[   `VX_MEM_TAG_WIDTH-1:0] mem_req_tag;
+ wire                           mem_req_ready;
+ // Memory response    
+ reg                         mem_rsp_valid;
+ reg[`VX_MEM_DATA_WIDTH-1:0] mem_rsp_data;
+ reg[ `VX_MEM_TAG_WIDTH-1:0] mem_rsp_tag;
+ wire                        mem_rsp_ready;
+ // DCR write request
+ wire                         dcr_wr_valid;
+ wire[`VX_DCR_ADDR_WIDTH-1:0] dcr_wr_addr;
+ wire[`VX_DCR_DATA_WIDTH-1:0] dcr_wr_data;
+ // Status
+ wire vx_busy;
+ // maxhpc
+ wire                           sim_ebreak;
+ wire[`NUM_REGS-1:0][`XLEN-1:0] sim_wb_value;
+ //
+ Vortex vortex(
+  // Clock
+  .clk  (sysclk)
+ ,.reset(vx_rst)
+ ,// Memory request
+  .mem_req_valid (mem_req_valid )
+ ,.mem_req_rw    (mem_req_rw    )
+ ,.mem_req_byteen(mem_req_byteen)
+ ,.mem_req_addr  (mem_req_addr  )
+ ,.mem_req_data  (mem_req_data  )
+ ,.mem_req_tag   (mem_req_tag   )
+ ,.mem_req_ready (mem_req_ready )
+ ,// Memory response    
+  .mem_rsp_valid(mem_rsp_valid)
+ ,.mem_rsp_data (mem_rsp_data )
+ ,.mem_rsp_tag  (mem_rsp_tag  )
+ ,.mem_rsp_ready(mem_rsp_ready)
+ ,// DCR write request
+  .dcr_wr_valid(dcr_wr_valid)
+ ,.dcr_wr_addr (dcr_wr_addr )
+ ,.dcr_wr_data (dcr_wr_data )
+ ,// Status
+  .busy(vx_busy)
+ ,// maxhpc
+  .sim_ebreak  (sim_ebreak  )
+ ,.sim_wb_value(sim_wb_value)
+ );
+/**/
+
 /* DDR3 */
- reg axi4_busy;
+ wire rcv_axi4_req;
+ reg  rcv_axi4_gnt;
+ //
+ wire rsp_axi4_req;
+ reg  rsp_axi4_gnt;
+ //
+ wire vx_axi4_req;
+ reg  vx_axi4_gnt;
 
  wire       axi4_awvalid;
   reg rcv_axi4_awvalid;
@@ -137,7 +199,9 @@ module maxhpc(
  wire[31:0] axi4_rdata;
  wire       axi4_rlast;
  wire       axi4_rvalid;
- ddr3_top ddr3(
+ ddr3_top #(
+  .DDR_MHZ(25)
+ ) ddr3(
  // clocks, reset
   .rst(!rst_)
  ,.clk(sysclk)
@@ -189,17 +253,22 @@ module maxhpc(
  ,.axi4_rready(1'b1       )
  );
 
+ reg axi4_busy;
  `ALWAYS(posedge sysclk, negedge rst_)
   if (!rst_) begin
    axi4_busy <= 1'b0;
+   //
+   rcv_axi4_gnt <= 1'b0;
+   rsp_axi4_gnt <= 1'b0;
+   vx_axi4_gnt  <= 1'b0;
   end
    else begin
     if (
-     axi4_awvalid
+     rcv_axi4_gnt
      ||
-     axi4_arvalid
+     rsp_axi4_gnt
      ||
-     mem_req_valid && mem_req_ready
+     vx_axi4_gnt
     ) begin
      axi4_busy <= 1'b1;
     end
@@ -209,6 +278,33 @@ module maxhpc(
      axi4_rvalid && axi4_rlast
     ) begin
      axi4_busy <= 1'b0;
+    end
+    //
+    //
+    vx_axi4_gnt  <= vx_axi4_gnt && !(mem_req_valid && mem_req_ready);
+    rsp_axi4_gnt <= rsp_axi4_gnt && !rsp_axi4_req;
+    rcv_axi4_gnt <= rcv_axi4_gnt && !rcv_axi4_req;
+    if (!rcv_axi4_gnt
+        &&
+        !rsp_axi4_gnt
+        &&
+        !vx_axi4_gnt
+    ) begin
+     vx_axi4_gnt <= !axi4_busy
+                    &&
+                    !rcv_axi4_req
+                    &&
+                    !rsp_axi4_req
+                    &&
+                    vx_axi4_req;
+     //
+     rsp_axi4_gnt <= !axi4_busy
+                     &&
+                     !rcv_axi4_req
+                     &&
+                     rsp_axi4_req;
+     //
+     rcv_axi4_gnt <= !axi4_busy && rcv_axi4_req;
     end
    end
 /**/
@@ -222,7 +318,7 @@ module maxhpc(
  wire     tx_idle;
 
  uart #(
-  .CLK_HZ(50000000)
+  .CLK_HZ(25000000)
  ,.BAUD  (1000000)
  ) uart(
   .rst_(rst_)
@@ -241,122 +337,70 @@ module maxhpc(
 /**/
 
 /* Vortex stuff */
- /* Vortex instance */
-  reg vx_rst;
-  // Memory request
-  wire                           mem_req_valid;
-  wire                           mem_req_rw;
-  wire[`VX_MEM_BYTEEN_WIDTH-1:0] mem_req_byteen;
-  wire[  `VX_MEM_ADDR_WIDTH-1:0] mem_req_addr;
-  wire[  `VX_MEM_DATA_WIDTH-1:0] mem_req_data;
-  wire[   `VX_MEM_TAG_WIDTH-1:0] mem_req_tag;
-  wire                           mem_req_ready;
-  // Memory response    
-  reg                         mem_rsp_valid;
-  reg[`VX_MEM_DATA_WIDTH-1:0] mem_rsp_data;
-  reg[ `VX_MEM_TAG_WIDTH-1:0] mem_rsp_tag;
-  wire                        mem_rsp_ready;
-  // DCR write request
-  wire                         dcr_wr_valid;
-  wire[`VX_DCR_ADDR_WIDTH-1:0] dcr_wr_addr;
-  wire[`VX_DCR_DATA_WIDTH-1:0] dcr_wr_data;
-  // Status
-  wire vx_busy;
-  // maxhpc
-  wire                           sim_ebreak;
-  wire[`NUM_REGS-1:0][`XLEN-1:0] sim_wb_value;
-  //
-  Vortex vortex(
-   // Clock
-   .clk  (sysclk)
-  ,.reset(vx_rst)
-  ,// Memory request
-   .mem_req_valid (mem_req_valid )
-  ,.mem_req_rw    (mem_req_rw    )
-  ,.mem_req_byteen(mem_req_byteen)
-  ,.mem_req_addr  (mem_req_addr  )
-  ,.mem_req_data  (mem_req_data  )
-  ,.mem_req_tag   (mem_req_tag   )
-  ,.mem_req_ready (mem_req_ready )
-  ,// Memory response    
-   .mem_rsp_valid(mem_rsp_valid)
-  ,.mem_rsp_data (mem_rsp_data )
-  ,.mem_rsp_tag  (mem_rsp_tag  )
-  ,.mem_rsp_ready(mem_rsp_ready)
-  ,// DCR write request
-   .dcr_wr_valid(dcr_wr_valid)
-  ,.dcr_wr_addr (dcr_wr_addr )
-  ,.dcr_wr_data (dcr_wr_data )
-  ,// Status
-   .busy(vx_busy)
-  ,// maxhpc
-   .sim_ebreak  (sim_ebreak  )
-  ,.sim_wb_value(sim_wb_value)
-  );
+ /* mem_req_/mem_rsp_ */
+  assign vx_axi4_req = mem_req_valid;
+  assign mem_req_ready = vx_axi4_gnt;
 
-  /* mem_req_/mem_rsp_ */
-   assign mem_req_ready = !axi4_busy;
-
-   reg      vx_axi4_read;
-   reg[5:0] vx_axi4_wCnt;
-    assign vx_axi4_wlast = vx_axi4_wCnt[5];
-   `ALWAYS(posedge sysclk, negedge rst_)
-    if (!rst_) begin
-     vx_axi4_awvalid <= 1'b0;
-     vx_axi4_wvalid <= 1'b0;
-     vx_axi4_arvalid <= 1'b0;
-     //
-     vx_axi4_read <= 1'b0;
-     vx_axi4_wCnt <= -1;
-     //
-     mem_rsp_valid <= 1'b0;
-    end
-     else begin
-      if (axi4_awready) begin
-       vx_axi4_awvalid <= 1'b0;
-      end
-      if (axi4_arready) begin
-       vx_axi4_arvalid <= 1'b0;
-      end
-      if (axi4_wready) begin
-       vx_axi4_wCnt <= vx_axi4_wCnt-1;
-       //
-       vx_axi4_wdata <= vx_axi4_wdata>>32;
-       vx_axi4_wstrb <= vx_axi4_wstrb>>4;
-       //
-       if (vx_axi4_wlast) begin
-        vx_axi4_wvalid <= 1'b0;
-       end
-      end
-      if (mem_req_valid && mem_req_ready) begin
-       vx_axi4_awvalid <= mem_req_rw;
-       vx_axi4_awaddr <= mem_req_addr<<(32-`VX_MEM_ADDR_WIDTH);
-       vx_axi4_wvalid <= mem_req_rw;
-       vx_axi4_wdata <= mem_req_data;
-       vx_axi4_wstrb <= mem_req_byteen;
-       //
-       vx_axi4_wCnt <= vx_axi4_awlen -1;
-       //
-       //
-       vx_axi4_arvalid <= !mem_req_rw;
-       vx_axi4_araddr <= mem_req_addr<<(32-`VX_MEM_ADDR_WIDTH);
-       mem_rsp_tag <= mem_req_tag;
-      end
-      if (vx_axi4_arvalid || rsp_axi4_arvalid) begin
-       vx_axi4_read <= vx_axi4_arvalid;
-      end
+  reg      vx_axi4_read;
+  reg[5:0] vx_axi4_wCnt;
+   assign vx_axi4_wlast = vx_axi4_wCnt[5];
+  `ALWAYS(posedge sysclk, negedge rst_)
+   if (!rst_) begin
+    vx_axi4_awvalid <= 1'b0;
+    vx_axi4_wvalid <= 1'b0;
+    vx_axi4_arvalid <= 1'b0;
+    //
+    vx_axi4_read <= 1'b0;
+    vx_axi4_wCnt <= -1;
+    //
+    mem_rsp_valid <= 1'b0;
+   end
+    else begin
+     if (vx_axi4_awvalid && axi4_awready) begin
+      vx_axi4_awvalid <= 1'b0;
+     end
+     if (vx_axi4_arvalid && axi4_arready) begin
+      vx_axi4_arvalid <= 1'b0;
+     end
+     if (axi4_wvalid && axi4_wready) begin
+      vx_axi4_wCnt <= vx_axi4_wCnt-1;
       //
+      vx_axi4_wdata <= vx_axi4_wdata>>32;
+      vx_axi4_wstrb <= vx_axi4_wstrb>>4;
       //
-      if (mem_rsp_ready) begin
-       mem_rsp_valid <= 1'b0;
-      end
-      if (axi4_rvalid) begin
-       mem_rsp_data[`VX_MEM_DATA_WIDTH-1-:32] <= axi4_rdata;
-       mem_rsp_data[0+:`VX_MEM_DATA_WIDTH-32] <= mem_rsp_data[`VX_MEM_DATA_WIDTH-1-:`VX_MEM_DATA_WIDTH-32];
-       //
-       mem_rsp_valid <= vx_axi4_read && axi4_rlast;
+      if (vx_axi4_wlast) begin
+       vx_axi4_wvalid <= 1'b0;
       end
      end
+     if (mem_req_valid && mem_req_ready) begin
+      vx_axi4_awvalid <= mem_req_rw;
+      vx_axi4_awaddr <= mem_req_addr<<(32-`VX_MEM_ADDR_WIDTH);
+      vx_axi4_wvalid <= mem_req_rw;
+      vx_axi4_wdata <= mem_req_data;
+      vx_axi4_wstrb <= mem_req_byteen;
+      //
+      vx_axi4_wCnt <= vx_axi4_awlen -1;
+      //
+      //
+      vx_axi4_arvalid <= !mem_req_rw;
+      vx_axi4_araddr <= mem_req_addr<<(32-`VX_MEM_ADDR_WIDTH);
+      mem_rsp_tag <= mem_req_tag;
+     end
+     if (vx_axi4_arvalid || rsp_axi4_arvalid) begin
+      vx_axi4_read <= vx_axi4_arvalid;
+     end
+     //
+     //
+     if (mem_rsp_ready) begin
+      mem_rsp_valid <= 1'b0;
+     end
+     if (axi4_rvalid) begin
+      mem_rsp_data[`VX_MEM_DATA_WIDTH-1-:32] <= axi4_rdata;
+      mem_rsp_data[0+:`VX_MEM_DATA_WIDTH-32] <= mem_rsp_data[`VX_MEM_DATA_WIDTH-1-:`VX_MEM_DATA_WIDTH-32];
+      //
+      mem_rsp_valid <= vx_axi4_read && axi4_rlast;
+     end
+    end
   /**/
  /**/
 
@@ -364,10 +408,12 @@ module maxhpc(
   enum {RCV_CMD
        ,RCV_CMD_STB
        ,RCV_AXI4_WDATA
+       ,RCV_AXI4_REQ
        ,RCV_AXI4_WRITE
        ,RCV_DCR_WRITE
        ,RCVST_SIZE} RCVST_ENUM;
   reg[RCVST_SIZE-1:0] rcv_state;
+   assign rcv_axi4_req = rcv_state[RCV_AXI4_REQ];
   struct packed{
    reg   [31:0] sizdat;
    reg   [31:0] addr;
@@ -376,6 +422,9 @@ module maxhpc(
    assign dcr_wr_valid = rcv_state[RCV_DCR_WRITE];
    assign dcr_wr_addr = rcv_cmd.addr;
    assign dcr_wr_data = rcv_cmd.sizdat;
+   //
+    wire[31:0] rcv_wtap_dat = rcv_cmd.sizdat;
+    reg  [3:0] rcv_wtap_stb;
   reg[6:0] rcv_cmdCnt;
   //
   reg[32:0] rcv_wrCnt;
@@ -403,6 +452,9 @@ module maxhpc(
       rcv_axi4_wvalid <= 1'b0;
       rcv_axi4_wstrb <= 4'b1111;
      end
+     //
+     rcv_wtap_stb <= 0;
+     //
      unique case (rcv_state)
       default: begin // (1<<RCV_CMD)
        if (rx_stb) begin
@@ -438,6 +490,10 @@ module maxhpc(
          vx_rst <= rcv_cmd.sizdat[0];
         end
         //
+        "tapW": begin
+         rcv_wtap_stb <= 1<<rcv_cmd.addr[1:0];
+        end
+        //
         default: begin
         end
        endcase
@@ -462,24 +518,28 @@ module maxhpc(
          end
         endcase
         if ((rcv_wrPtr==2'h3) || rcv_wrCnt[32]) begin
-         rcv_state <= (1<<RCV_AXI4_WRITE);
+         rcv_state <= (1<<RCV_AXI4_REQ);
         end
        end
       end
       //
+      (1<<RCV_AXI4_REQ): begin
+       if (rcv_axi4_gnt) begin
+        rcv_state <= (1<<RCV_AXI4_WRITE);
+       end
+      end
+      //
       (1<<RCV_AXI4_WRITE): begin
-       if (!axi4_busy) begin
-        rcv_axi4_awvalid <= 1'b1;
+       rcv_axi4_awvalid <= 1'b1;
+       //
+       rcv_axi4_wvalid <= 1'b1;
+       //
+       rcv_state <= (1<<RCV_AXI4_WDATA);
+       rcv_cmdCnt <= ($bits(rcv_cmd)+7)/8 -2;
+       if (rcv_wrCnt[32] && !rcv_wrCnt[0]) begin
+        rcv_axi4_wstrb  <= rcv_axi4_wstrb & (4'b1111>>(2'h3 & ~(rcv_cmd.addr[1:0]+rcv_cmd.sizdat[1:0]-1)));
         //
-        rcv_axi4_wvalid <= 1'b1;
-        //
-        rcv_state <= (1<<RCV_AXI4_WDATA);
-        rcv_cmdCnt <= ($bits(rcv_cmd)+7)/8 -2;
-        if (rcv_wrCnt[32] && !rcv_wrCnt[0]) begin
-         rcv_axi4_wstrb  <= rcv_axi4_wstrb & (4'b1111>>(2'h3 & ~(rcv_cmd.addr[1:0]+rcv_cmd.sizdat[1:0]-1)));
-         //
-         rcv_state <= (1<<RCV_CMD);
-        end
+        rcv_state <= (1<<RCV_CMD);
        end
       end
       //
@@ -492,18 +552,21 @@ module maxhpc(
  /**/
  /* respond */
   enum {RSP_IDLE
-       ,RSP_AXI4_READ
+       ,RSP_AXI4_REQ
        ,RSP_AXI4_ARVALID
        ,RSP_AXI4_RVALID
        ,RSP_AXI4_RDATA
        ,RSP_TAP_READ
        ,RSPST_SIZE} RSPST_ENUM;
   reg[RSPST_SIZE-1:0] rsp_state;
+   assign rsp_axi4_req = rsp_state[RSP_AXI4_REQ];
    assign rsp_axi4_arvalid = rsp_state[RSP_AXI4_ARVALID];
   reg[31:0] rsp_word;
   reg[32:0] rsp_byteCnt;
   reg [1:0] rsp_axi4rdPtr;
-  reg[31:0] rsp_taps[0:3];
+  //
+  logic[31:0] rsp_rtaps[0:3];
+  reg   [3:0] rsp_rtap_stb;
   `ALWAYS(posedge sysclk, negedge rst_)
    if (!rst_) begin
     tx_req <= 1'b0;
@@ -514,6 +577,9 @@ module maxhpc(
      if (!tx_idle) begin
       tx_req <= 1'b0;
      end
+     //
+     rsp_rtap_stb <= 0;
+     //
      unique case (rsp_state)
       default: begin // (1<<RSP_IDLE)
        if (rcv_state[RCV_CMD_STB]) begin
@@ -524,12 +590,13 @@ module maxhpc(
           rsp_byteCnt <= rcv_cmd.sizdat -2;
           rsp_axi4rdPtr <= rcv_cmd.addr[1:0];
           //
-          rsp_state <= (1<<RSP_AXI4_READ);
+          rsp_state <= (1<<RSP_AXI4_REQ);
          end
          //
          "tapR": begin
           rsp_byteCnt <= ($bits(rsp_word)+7)/8 -2;
-          rsp_word <= rsp_taps[rcv_cmd.addr[1:0]];
+          rsp_word <= rsp_rtaps[rcv_cmd.addr[1:0]];
+          rsp_rtap_stb <= 1<<rcv_cmd.addr[1:0];
           //
           rsp_state <= (1<<RSP_TAP_READ);
          end
@@ -540,8 +607,8 @@ module maxhpc(
        end
       end
       //
-      (1<<RSP_AXI4_READ): begin
-       if (!axi4_busy) begin
+      (1<<RSP_AXI4_REQ): begin
+       if (rsp_axi4_gnt) begin
         rsp_state <= (1<<RSP_AXI4_ARVALID);
        end
       end
@@ -572,7 +639,7 @@ module maxhpc(
         tx_req <= 1'b1;
         //
         if (rsp_axi4rdPtr==2'h3) begin
-         rsp_state <= (1<<RSP_AXI4_ARVALID);
+         rsp_state <= (1<<RSP_AXI4_REQ);
         end
         if (rsp_byteCnt[32]) begin
          rsp_state <= (1<<RSP_IDLE);
@@ -603,15 +670,121 @@ module maxhpc(
  assign {led_rgb3,led_rgb2,led_rgb1,led_rgb0} = {{3{1'b1}},{3{1'b1}},{3{vx_rst}},{3{!vx_busy}}};
 /**/
 
-/* rsp_taps */
+/* taps */
+ reg[10:0] stap_wadr;
+ reg[31:0] stap_wdat[0:1];
+ //
+ reg[10:0] stap_radr;
+ maxhpc_dpram #(
+  .ADDR_WD(10)
+ ,.DATA_WD(32)
+ ) stap_ram(
+ /* Port A */
+  .a_clk (sysclk)
+ ,.a_ce  (!stap_wadr[10])
+ ,.a_we  (!stap_wadr[10])
+ ,.a_addr(stap_wadr[9:0])
+ ,.a_d   (stap_wdat[1])
+ ,.a_q   ()
+ /**/
+ ,
+ /* Port B */
+  .b_clk (sysclk)
+ ,.b_ce  (!stap_radr[10])
+ ,.b_we  (1'b0)
+ ,.b_addr(stap_radr[9:0])
+ ,.b_d   ()
+ ,.b_q   (rsp_rtaps[3])
+ /**/
+ );
+
+ reg stap_run;
+ reg stap_trig;
+ reg[9:0] stap_trigAdr;
+  assign rsp_rtaps[2] = stap_trigAdr;
+ reg[4:0] tttCnt;
  `ALWAYS(posedge sysclk, negedge rst_)
   if (!rst_) begin
-   rsp_taps[0] <= 32'h00000000;
-   rsp_taps[1] <= 32'h00000000;
-   rsp_taps[2] <= 32'h00000000;
-   rsp_taps[3] <= 32'h00000000;
+   stap_run <= 1'b1;
+   //stap_wadr[10] <= 1'b1;
+   stap_wadr <= 0;
+   //
+   stap_run <= 1'b1;
+   stap_trig  <= 1'b0;
+tttCnt <= 0;
   end
    else begin
+    /* */
+    //if (!stap_wadr[10]) begin
+    // stap_wadr <= stap_wadr[9:0]+1;
+    //end
+    //if (stap_run && stap_trig) begin
+    // stap_run <= 1'b0;
+    // //
+    // stap_wadr[10] <= 1'b0;
+    //end
+    if (!stap_wadr[10]) begin
+     stap_wadr[9:0] <= stap_wadr[9:0]+1;
+    end
+    if (stap_run && stap_trig) begin
+     stap_run <= 1'b0;
+     //
+     stap_trigAdr <= stap_wadr[9:0];
+    end
+    if (!stap_run) begin
+     stap_wadr[10] <= (stap_wadr[9:0]-stap_trigAdr[9:0]+1)>>(10-1);
+    end
+    /**/
+    //
+    stap_trig  <= tttCnt[4];
+tttCnt <= tttCnt+1;
+if (!vx_axi4_awvalid) begin
+ tttCnt <= 0;
+end
+    stap_wdat[0] <= {                             axi4_rlast,    axi4_rvalid
+                    ,vx_axi4_arvalid, axi4_wlast, axi4_wready,   vx_axi4_wvalid
+                    ,vx_axi4_awvalid, mem_req_rw, mem_req_ready, mem_req_valid
+                    };
+    stap_wdat[1] <= stap_wdat[0];
+    if (rcv_wtap_stb[2]) begin
+     stap_run <= 1'b1;
+     stap_trig <= 1'b0;
+     stap_wadr[10] <= 1'b0;
+    end
+   end
+
+ `ALWAYS(posedge sysclk, negedge rst_)
+  if (!rst_) begin
+   stap_radr <= 0;
+  end
+   else begin
+    if (rsp_rtap_stb[3]) begin
+     //if (!stap_radr[10]) begin
+     // stap_radr <= stap_radr[9:0]+1;
+     //end
+     stap_radr[9:0] <= stap_radr[9:0]+1;
+    end
+    if (rcv_wtap_stb[3]) begin
+     stap_radr <= rcv_wtap_dat;
+    end
+   end
+ //
+ //
+ `ALWAYS(posedge sysclk, negedge rst_)
+  if (!rst_) begin
+   rsp_rtaps[0] <= 32'h00000000;
+  end
+   else begin
+    rsp_rtaps[0] <= {stap_run, axi4_busy, vx_axi4_arvalid, vx_axi4_wvalid, vx_axi4_awvalid};
+    rsp_rtaps[0][23:16] <= rsp_rtaps[0][23:16]
+                          +(mem_req_valid && mem_req_ready && mem_req_rw)
+                          -(vx_axi4_wvalid && axi4_wready && axi4_wlast);
+    rsp_rtaps[0][31:24] <= rsp_rtaps[0][31:24]
+                          +(mem_req_valid && mem_req_ready && !mem_req_rw)
+                          -(vx_axi4_read && axi4_rvalid && axi4_rlast);
+    if (vx_rst) begin
+     rsp_rtaps[0][31:16] <= 0;
+    end
    end
 /**/
 
--- vx/vx_stub.sv
+++ vx/vx_stub.sv
@@ -10,7 +10,7 @@ module Vortex import VX_gpu_pkg::*; (
 ,input reset
 ,// Memory request
  output logic                      mem_req_valid
-,output                            mem_req_rw
+,output logic                      mem_req_rw
 ,output[`VX_MEM_BYTEEN_WIDTH-1:0]  mem_req_byteen
 ,output[  `VX_MEM_ADDR_WIDTH-1:0]  mem_req_addr
 ,output[  `VX_MEM_DATA_WIDTH-1:0]  mem_req_data
@@ -20,7 +20,7 @@ module Vortex import VX_gpu_pkg::*; (
  input                         mem_rsp_valid
 ,input[`VX_MEM_DATA_WIDTH-1:0] mem_rsp_data
 ,input[ `VX_MEM_TAG_WIDTH-1:0] mem_rsp_tag
-,output                        mem_rsp_ready
+,output logic                  mem_rsp_ready
 ,// DCR write request
  input                         dcr_wr_valid
 ,input[`VX_DCR_ADDR_WIDTH-1:0] dcr_wr_addr
@@ -33,21 +33,52 @@ module Vortex import VX_gpu_pkg::*; (
 );
 
 `ifndef VERILATOR
-assign mem_req_valid = 1'b0;
-assign mem_rsp_ready = 1'b0;
+assign mem_rsp_ready = mem_rsp_valid;
+
+assign mem_req_addr = 'h100;
+//assign mem_req_rw = 1'b1;
+assign mem_req_byteen = {4'h0,4'h2,4'h3,4'h4,4'h5,4'h6,4'h7,4'h8
+                        ,4'h9,4'hA,4'hB,4'hC,4'hD,4'hE,4'hF,4'h1
+                        };
+assign mem_req_data = {32'h11111111,32'h22222222,32'h33333333,32'h44444444,32'h55555555,32'h66666666,32'h77777777,32'h88888888
+                      ,32'h99999999,32'hAAAAAAAA,32'hBBBBBBBB,32'hCCCCCCCC,32'hDDDDDDDD,32'hEEEEEEEE,32'hFFFFFFFF,32'h12345678
+                      };
+
+always @(posedge clk, posedge reset)
+ if (reset) begin
+  mem_req_valid <= 1'b0;
+  mem_req_rw <= 1'b0;
+ end
+  else begin
+   mem_req_valid <= 1'b1;
+   //
+   if (mem_req_valid && mem_req_ready) begin
+    mem_req_rw <= !mem_req_rw;
+   end
+  end
 
 `else
 assign mem_rsp_ready = mem_rsp_valid;
 
 reg[32:0] mem_reqCnt = 65000;
-// assign mem_req_valid = mem_reqCnt[32];
- assign mem_req_addr = 'h100;
- assign mem_req_rw = 1'b0;
- assign mem_req_byteen = {`VX_MEM_BYTEEN_WIDTH{1'b1}};
+ //assign mem_req_valid = mem_reqCnt[32];
+// assign mem_req_addr = 'h100;
+ assign mem_req_addr = 'hFFFFFFFF;
+// assign mem_req_rw = 1'b1;
+ assign mem_req_byteen = {4'h0,4'h2,4'h3,4'h4,4'h5,4'h6,4'h7,4'h8
+                         ,4'h9,4'hA,4'hB,4'hC,4'hD,4'hE,4'hF,4'h1
+                         };
+ assign mem_req_data = {32'h11111111,32'h22222222,32'h33333333,32'h44444444,32'h55555555,32'h66666666,32'h77777777,32'h88888888
+                       ,32'h99999999,32'hAAAAAAAA,32'hBBBBBBBB,32'hCCCCCCCC,32'hDDDDDDDD,32'hEEEEEEEE,32'hFFFFFFFF,32'h12345678
+                       };
 always @(posedge clk) begin
  mem_reqCnt <= mem_reqCnt[31:0]-1;
  if (mem_reqCnt[32]) begin
   mem_req_valid <= 1'b1;
+ end
+ //
+ if (mem_req_valid && mem_req_ready) begin
+  mem_req_rw <= !mem_req_rw;
  end
 end
 
